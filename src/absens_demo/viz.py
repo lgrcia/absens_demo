f51fc17ec01fd8538c1f452d7561df06b7561740
lgrcia <lionel_garcia@live.fr>
2026-03-04 12:41:52 +0100

    feat: matplotlib-based plotting function for the video feature

--- src/absens_demo/viz.py
+++ src/absens_demo/viz.py
@@ -1,6 +1,8 @@
 import cv2
 import logging
 from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
 
 from .io import load_npy
 
@@ -26,3 +28,24 @@ def make_video(image_folder: Path, output_path: Path, fps: int = 1):
 
     video.release()
     logger.info(f"Video saved to {output_path}")
+
+
+def plot_function(raw, aligned):
+    def plot_rgb_clm(data, ax):
+        ax.imshow(data["rgb"])
+        clm_alpha = np.where(data["clm"] > 0, 1, np.nan)
+        # draw contour around clouds
+        ax.contour(
+            data["clm"],
+            levels=[0.5],
+            colors="yellow",
+            linewidths=0.5,
+            alpha=1,
+        )
+
+    _, axes = plt.subplots(1, 2, figsize=(10, 5), sharey=True, sharex=True)
+    plot_rgb_clm(raw, axes[0])
+    axes[0].set_title("Raw Image")
+    plot_rgb_clm(aligned, axes[1])
+    axes[1].set_title("Aligned Image")
+    plt.tight_layout()
